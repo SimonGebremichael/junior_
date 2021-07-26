7078d41d8d08efb495a18f6130a4d82c3db015b9
simon g <simong8080@hotmail.com>
2021-07-25 22:00:08 -0400

    new design

--- src/index.js
+++ src/index.js
@@ -2,13 +2,11 @@ import React from "react";
 import ReactDOM from "react-dom";
 import { BrowserRouter, Route, Switch } from "react-router-dom";
 import Home from "./Home";
-import PageView from "./pageView";
 
 function Start() {
   return (
     <BrowserRouter>
       <Switch>
-        <Route path="/pageview" component={PageView} />
         <Route path="*" component={Home} />
       </Switch>
     </BrowserRouter>
