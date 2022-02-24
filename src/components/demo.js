7da792f3ed74311af42f954505f791d97065c4bd
simon_g_ <simon@mmglobalsolutionsconsulting.com>
2022-02-23 22:05:36 -0500

    pics update

--- src/components/demo.js
+++ src/components/demo.js
@@ -4,37 +4,20 @@ export default function start() {
   return (
     <div id="preview" className="section">
       <div className="container previews" id="1">
-        <img src="imgs/pic35.jpg" alt="Before" />
-        <h3>Before</h3>
+        <img src="imgs/pr_pic1.jpg" alt="Before" />
+        <h3>Previous customers</h3>
       </div>
       <div className="container previews" id="2">
-        <img src="imgs/pic34.jpg" />
-        <h3>After</h3>
+        <img src="imgs/pr_pic2.jpg" />
       </div>
       <div className="container previews" id="3">
-        <img src="imgs/pic15.jpg" />
-        <h3>Before</h3>
+        <img src="imgs/pr_pic3.jpg" />
       </div>
       <div className="container previews" id="4">
-        <img src="imgs/pic25.jpg" />
-        <h3>After</h3>
+        <img src="imgs/pr_pic4.jpg" />
       </div>
-      <div id="preData">
-        <h2>
-          Begining at &nbsp;
-          <font color="lightgreen">
-            $12.<sup>99</sup>/SQ Foot
-          </font>
-        </h2>
-        <br />
-        <p>
-          Top quality and long lasting tiles made for your satisfaction. Book a
-          Flooring service now.
-        </p>
-        <br />
-        <a href="#form">
-          <button>Book</button>
-        </a>
+      <div className="container previews" id="4">
+        <img src="imgs/pr_pic5.jpg" />
       </div>
     </div>
   );
